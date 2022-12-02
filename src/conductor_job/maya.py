35002bbca91f6461b7dc73f0b41d686cbb0ba11d
Jesse Lehrman <jesse@conductortech.com>
2022-12-02 10:55:43 -0500

    Uses the OutputFilePath Plugin key for the output path

--- src/conductor_job/maya.py
+++ src/conductor_job/maya.py
@@ -81,7 +81,9 @@ class MayaRenderJob(job.Job):
             args = ""
                 
             if renderer == "arnold-maya":
-                args = "-ai:lve {log_level}".format(log_level=self.log_level)
+                # Use the same prefix as the Maya project path (ex: /projects/my_project)
+                standin_path = "/".join(ciopath.gpath.Path(self.project_path).fslash(with_drive=False).split("/")[0:3])
+                args = "-ai:lve {log_level} -ai:sptx {}".format(log_level=self.log_level, standin_path)
                 
             return args
         
